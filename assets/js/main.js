d005d5ed92ae33a3eda8c2844d788d7d8e64782a
Rafa Kabuchi <rkabuchi@gmail.com>
2021-03-07 19:42:37 -0300

    certificado criado no curso da Alura

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+
+function gerar() {
+    contador=1000000
+    while(contador>0){
+      contador=contador-1
+      certificado=emitirCertificado(inputNome.value)}
+   
+  
+    document.body.innerHTML= 
+    `${certificado}
+  <ol>
+  <li><a href="https://codepen.io/rafakabuchi/pen/abOxabg">Calculadora de Corridas</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/LYVoOvo">Curriculo</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/PoqvQWd">App de transporte</a></li>
+  <li><a href="https://codepen.io/MGBFAS/pen/ExjzoLY">Currículo</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/vYOwdeR">Master Ball</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/vYOwdeR"></a>Ficha</li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/GRJVeOw">Qual o mês?</a></li>
+  <li><a href="https://codepen.io/MGBFAS/pen/XWbvJyO">Ficha</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/vYOoMeB">Calculadora do IMC</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/eYNqqyM">Corrida Grátis</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/ZEbzPVO">Contador de Likes</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/BaoBXdy">Contador de Olímpiadas</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/abvbzdV">De 1 a 500</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/qBOBEGv">Números pares de 1 à 100</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/abvbVzO">Corrida Grátis 2.0</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/ExVxdyO">Ficha de Inscrição</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/qBOBLMM">Conversor de Temperatura</a></li>
+  <li><a href="https://codepen.io/rafakabuchi/pen/abvzZGN">Certificado</a></li>
+  </ol>
+  `
+    window.print()
+  }
